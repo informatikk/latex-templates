12bde0f07792b97745a0fc74432df3d138f19e2b
Pascal Weiland <weiland@users.noreply.github.com>
2020-04-19 19:07:52 +0200

    Add update script

--- scripts/copy_files.sh
+++ scripts/copy_files.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
-cp tuda_latex_templates/tex/{*.sty, *.cls, *.def, *.clo, *.cfg} ./
+cp tuda_latex_templates/tex/* ./
+
---
+++ scripts/update.sh
@@ -0,0 +1,6 @@
+sh scripts/reset_changes.sh
+sh scripts/update_submodule.sh
+sh scripts/add_license.sh
+sh scripts/copy_files.sh
+sh scripts/reset_changes.sh
+sh scripts/download_logo.sh
